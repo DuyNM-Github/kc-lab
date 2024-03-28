a447b5cba5951324a39f723feaedc4754b2eea3c
DuyNM <duy.1icecoffee@gmail.com>
2024-03-28 16:25:09 +0700

    update poc and sample files

--- main.py
+++ main.py
@@ -12,14 +12,12 @@ kc_handler = KeycloakOpenID(server_url="http://localhost:8080/",
                                  client_id="flaskr",
                                  realm_name="duyrealm",
                                  client_secret_key='y5WWBskC8ZTRyE94mhKGW36XcCwf1vUq')
-config_well_know = kc_handler.well_known()
 
 
 @app.middleware("http")
 async def authorize(request: Request, call_next):
     path = urlparse(str(request.url)).path
     path_compositions = path.split("/")[1:]
-    print(path_compositions)
 
     if re.search("^\/auth\/[\w\/]+", path) is None:
 
@@ -31,18 +29,29 @@ async def authorize(request: Request, call_next):
         scopes = path_compositions[1:] if len(path_compositions) > 1 else None
         print("/".join(scopes))
 
-        # print(kc_handler.uma_permissions(token))
         perm = f"/{resource}"
         if scopes is not None:
             perm = "#".join((perm, f"/{'/'.join(scopes)}"))
-        print(perm)
-        print(kc_handler.has_uma_access(token, permissions="Empty"))
 
+        try:
+            evaluation = kc_handler.has_uma_access(token, permissions=perm)
+            print(evaluation)
+        except KeycloakPostError as err:
+            return Response(status_code=401)
+
+        if not evaluation.is_authorized or not evaluation.is_logged_in:
+            return Response(status_code=401)
+            
 
     response = await call_next(request)
     return response
 
 
-@app.get("/qa/somethingelse/otherthing")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/auth/login")
+async def login():
+    return {"message": "Log In"}
+
+
+@app.get("/gene/data")
+async def data():
+    return {"message": "Get gene data"}
